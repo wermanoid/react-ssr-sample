505e98d98b325f3175095772040e94ccae13d938
wermanoid <wermanoid@gmail.com>
2017-11-20 10:03:29 +0100

    fix linter

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,14 +1,16 @@
 import path from 'path';
+import { HotModuleReplacementPlugin } from 'webpack';
 import resolver from './webpack.config.resolve';
 
 const DIST_DIR = path.resolve(__dirname, 'dist');
 
 export default () => ({
   ...resolver,
-  entry:  [
-    'webpack-dev-server/client?http://127.0.0.1:8080/',
-    'webpack/hot/only-dev-server',
-    './src/index.jsx'
+  entry: [
+    // 'webpack-dev-server/client?http://127.0.0.1:8080/',
+    // 'webpack/hot/only-dev-server',
+    'webpack-hot-middleware/client?reload=true',
+    './src/index.jsx',
   ],
   output: {
     path: DIST_DIR,
@@ -20,7 +22,10 @@ export default () => ({
         test: /.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-      }
-    ]
-  }
+      },
+    ],
+  },
+  plugins: [
+    new HotModuleReplacementPlugin(),
+  ],
 });
