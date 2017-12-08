523f671a4da6d4938d12fda2ace5d3ffd2bf1c5b
wermanoid <wermanoid@gmail.com>
2017-12-08 10:38:52 +0100

    added jest

--- src/components/atom/Button/Button.jsx
+++ src/components/atom/Button/Button.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import Btn from 'material-ui/Button';
 
 type PropsType = {
-  children: React$Node,
+  children?: React$Node,
 }
 
 const Button = ({ children }: PropsType) => (
   <Btn onClick={null}>{ children || 'sample'}</Btn>
 );
+
+Button.defaultProps = {
+  children: null,
+};
 
 export default Button;
---
+++ src/components/atom/Button/Button.spec.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Button from './Button';
+
+describe('<Button /> tests', () => {
+  test('mount properly', () => {
+    const sut = mount(<Button />);
+    expect(sut).toBeDefined();
+    expect(toJson(sut)).toMatchSnapshot();
+  });
+});
