b55a7a169f466a39b62d076543d3fa33405ccaea
wermanoid <wermanoid@gmail.com>
2017-12-08 10:49:56 +0100

    add theming for sc

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from '#components/App';
 import * as Pages from './loader';
 
@@ -10,14 +11,16 @@ const switchMapState = ({ location }) => ({ location });
 const ConnectedSwitch = connect(switchMapState, null)(Switch);
 
 const Routes = () => (
-  <App>
-    <ConnectedSwitch>
-      <Route exact path="/" component={Pages.HomePage} />
-      <Route path="/home" component={Pages.HomePage} />
-      <Route path="/sample" component={Pages.SamplePage} />
-      <Route path="*" component={Pages.NotFoundPage} />
-    </ConnectedSwitch>
-  </App>
+  <ThemeProvider theme={{}}>
+    <App>
+      <ConnectedSwitch>
+        <Route exact path="/" component={Pages.HomePage} />
+        <Route path="/home" component={Pages.HomePage} />
+        <Route path="/sample" component={Pages.SamplePage} />
+        <Route path="*" component={Pages.NotFoundPage} />
+      </ConnectedSwitch>
+    </App>
+  </ThemeProvider>
 );
 
 export default Routes;
